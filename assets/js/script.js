bfca333b3759f26fcc321807246abc3c06316fb7
James D <jtd.programming@gmail.com>
2022-01-20 19:27:44 -0500

    js pseudo mapped, TODO: js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-//write script to populate editable scheduler within "container" class
+// TODO: Use Acceptance Criteria to map out necessary code blocks
+// Review CSS for style classes to apply to HTML
+
+// function to display current day at top of app using #currentDay and moment.js
+
+
+// time block grid for standard business hours 9am-5pm (color code based on past/present/future)
+
+
+// function to load any events from events array in local storage. If nothing in localStorage init array
+
+
+// onclick in time block - edit event for that hour and save to localStorage - overwrite existing text
+
+
+// refresh page every 30 minutes if left open
